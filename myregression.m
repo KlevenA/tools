d64e9d183d4154649eb117dbf8d761570ea4ead9
KlevenA <107913047+KlevenA@users.noreply.github.com>
2026-04-08 11:25:02 +0200

    Update myregression.m

--- myregression.m
+++ myregression.m
@@ -1,4 +1,5 @@
 function [r, m, b] = regression(t, y, ~)
+% Simple function for linear regression that requires no additional packages grr
     % Compute slope (m) and intercept (b)
     p = polyfit(t, y, 1);
     m = p(1);
